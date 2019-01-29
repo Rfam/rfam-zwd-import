9a0ff2d2131aa738e87a5bef24cce49749d7a764
Anton Petrov <apetrov@ebi.ac.uk>
2019-01-29 17:19:23 +0000

    Add a helper script for converting ZWD to fasta

---
+++ convert-to-fasta.py
@@ -0,0 +1,13 @@
+
+import fnmatch
+import os
+
+os.system('rm zwd.fasta')
+
+matches = []
+for root, dirnames, filenames in os.walk('.'):
+    for filename in fnmatch.filter(filenames, '*.sto'):
+        matches.append(os.path.join(root, filename))
+        cmd = 'esl-reformat fasta {} >> zwd.fasta'.format(os.path.join(root, filename))
+        print(cmd)
+        os.system(cmd)
