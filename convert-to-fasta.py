dc79fc26b611b019f3d0411601fa3cc2d52120d9
Anton Petrov <apetrov@ebi.ac.uk>
2021-05-10 11:34:50 +0100

    Update fasta conversion script

--- convert-to-fasta.py
+++ convert-to-fasta.py
@@ -5,7 +5,7 @@ import os
 os.system('rm zwd.fasta')
 
 matches = []
-for root, dirnames, filenames in os.walk('.'):
+for root, dirnames, filenames in os.walk('/path/to/zashaweinbergdata/'):
     for filename in fnmatch.filter(filenames, '*.sto'):
         matches.append(os.path.join(root, filename))
         cmd = 'esl-reformat fasta {} >> zwd.fasta'.format(os.path.join(root, filename))
